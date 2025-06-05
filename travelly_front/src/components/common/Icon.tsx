b60f817c5ae1c339904891ab1c1aa3c743375bae
ParkTjgus <129525514+ParkTjgus@users.noreply.github.com>
2025-06-05 20:36:17 +0900

    feat: 공통 Icon 컴포넌트 구축

---
+++ travelly_front/src/components/common/Icon.tsx
@@ -0,0 +1,21 @@
+import type { IconType } from "react-icons";
+import * as Fa6Icons from "react-icons/fa6";
+
+interface IconProps {
+  iconName: string;
+  size?: number;
+  color?: string;
+}
+
+const IconList: Record<string, keyof typeof Fa6Icons> = {};
+
+const Icon = (props: IconProps) => {
+  const iconName = IconList[props.iconName];
+  let IconComponent: IconType | undefined = Fa6Icons[iconName];
+
+  if (!IconComponent) return null;
+
+  return <IconComponent size={props.size} color={props.color} />;
+};
+
+export default Icon;
