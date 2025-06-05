42b4ecb95ed1531bde0f5ff54e93681960bf29a8
ParkTjgus <129525514+ParkTjgus@users.noreply.github.com>
2025-06-05 21:31:04 +0900

    chore: 기본 style 삭제

---
+++ travelly_front/src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Login, Register } from "./pages";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
